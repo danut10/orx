2107e2f8585e42bddb3f84458bd14cc5d0cffdfb
Dan Maxim <dan.maxim@cegeka.com>
2018-09-18 19:28:39 +0300

    init: menu

--- orx-ng/src/app/components/app/app.component.ts
+++ orx-ng/src/app/components/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import 'metismenu';
+
+// declare let $: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Open RobotX - User Interface';
+	title = 'Open RobotX - User Interface';
+	
+	ngAfterViewInit() {
+		//
+		$('#menu').metisMenu();
+		
+	}	
 
 }
