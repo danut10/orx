f10208842e5a35aa241c5ca9d45ec7d0964fd1c3
Dan Maxim <dan.maxim@cegeka.com>
2018-11-05 11:38:30 +0200

    init: web-1 app

--- orx-web-1/src/main/java/ro/jdmx/orx/web1/core/DbConfiguration.java
+++ orx-web-1/src/main/java/ro/jdmx/orx/web1/core/DbConfiguration.java
@@ -4,20 +4,22 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 @Configuration
+@MapperScan("ro.jdmx.orx.app.dao")
 public class DbConfiguration {
 
-    @Autowired
-    private Environment environment;
+    @Autowired private Environment environment;
+    @Autowired private ResourceLoader resourceLoader;
     
-	@Bean(name="dataSource")
-	@Primary
+    @Bean(name="dataSource")
 	public DataSource dataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
@@ -29,10 +31,15 @@ public class DbConfiguration {
 	
 	@Bean(name = "sqlSessionFactory")
 	public SqlSessionFactoryBean sqlSessionFactory() {
+		Resource configLocation = resourceLoader.getResource("classpath:conf/mybatis.xml");
+		Resource[] mapperLocations = { resourceLoader.getResource("classpath:mybatis/mappers/*.xml") };
+		String typeAliassesPackage = "ro.jdmx.orx.common.dataRecords";
+		
 		final SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setDataSource(dataSource());
-		sqlSessionFactoryBean.setConfigLocation(null);
-		sqlSessionFactoryBean.setMapperLocations(null);
+		sqlSessionFactoryBean.setConfigLocation(configLocation);
+		sqlSessionFactoryBean.setMapperLocations(mapperLocations);
+		sqlSessionFactoryBean.setTypeAliasesPackage(typeAliassesPackage);
 		return sqlSessionFactoryBean;
 	 }
 	
